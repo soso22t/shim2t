02ba25c7b68e301d679946a9dec99820f6087593
skkl22r-prog <skkl22r@gmail.com>
2026-08-17 12:33:43 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Heart, Calendar } from "lucide-react";
-import invitationImg from "@/assets/RA.jpeg";
+import invitationImg from "@/assets/1.png";
 import sosImg from "@/assets/Rn.jpeg";
 
 import footerBgImg from "@/assets/96AF05E8-7D83-48B7-B124-4763797873E0.png";
