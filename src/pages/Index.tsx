f8a23ff429659ee363766dc97e8da062dbfa80ec
skkl22r-prog <skkl22r@gmail.com>
2026-08-22 12:04:16 +0300

    Change sosImg file extension from jpeg to png

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Heart, Calendar } from "lucide-react";
 import invitationImg from "@/assets/B.png";
-import sosImg from "@/assets/xx.jpeg";
+import sosImg from "@/assets/xx.png";
 
 import footerBgImg from "@/assets/96AF05E8-7D83-48B7-B124-4763797873E0.png";
 import cardImg from "@/assets/IMG_5716.jpeg";
