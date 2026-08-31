d86d740faf59e246d1473e440d88fff2a64e999b
skkl22r-prog <skkl22r@gmail.com>
2026-09-01 01:40:12 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import Scan from "./pages/Scan.tsx";
+import Manage from "./pages/Manage.tsx";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/scan/:token" element={<Scan />} />
+          <Route path="/manage/:id" element={<Manage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
