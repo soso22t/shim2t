33d52c3d7629cbb9c98e04e227ac41408206acff
skkl22r-prog <skkl22r@gmail.com>
2026-09-01 02:14:10 +0300

    Update Manage.tsx

--- src/pages/Manage.tsx
+++ src/pages/Manage.tsx
@@ -1,4 +1,4 @@
-وذا import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { supabase } from "@/integrations/supabase/client";
 
